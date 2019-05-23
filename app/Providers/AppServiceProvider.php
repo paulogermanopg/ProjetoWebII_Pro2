11f6071ce681dc401b58ec07ef0fbf0ec98811e0
Paulo Germano <paulogermano.123pg@hotmail.com>
2019-05-23 13:22:59 -0300

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        \URL::forceSchema('https');
+        \URL::forceScheme('https');
         //
     }
 }
