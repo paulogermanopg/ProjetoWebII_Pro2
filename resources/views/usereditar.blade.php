57d5d43d7a12db10211618a8502099f18ee473ab
Paulo Germano <paulogermano.123pg@hotmail.com>
2019-05-24 01:01:22 -0300

    finalmente

--- resources/views/usereditar.blade.php
+++ resources/views/usereditar.blade.php
@@ -3,36 +3,37 @@
 @section('content')
     <div class="container">
         @forelse($User as $u)
-            <div class="row">
-                <div class="col-sm-2">
+            @if($u->email != 'admin@admin.com')
+                <div class="row">
+                    <div class="col-sm-2">
+
+                    </div>
+                    <div class="col-sm-7">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Categoria</th>
+                                <th scope="col">Mudar Categoria</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">{{$u->id}}</th>
+                                <td>{{$u->name}}</td>
+                                <td>{{$u->email}}</td>
+                                <td>{{$u->id_adm}}</td>
+                                <td><a href="{{url("/livros/user/$u->id/mudar")}}" class="btn btn-primary">Alterar Status</a></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
                 </div>
-                <div class="col-sm-7">
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Categoria</th>
-                            <th scope="col">Mudar Categoria</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="row">{{$u->id}}</th>
-                            <td>{{$u->name}}</td>
-                            <td>{{$u->email}}</td>
-                            <td>{{$u->id_adm}}</td>
-                            <td><a href="{{url("/livros/user/$u->id/mudar")}}" class="btn btn-primary">Alterar Status</a></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-            </div>
-
-
+            @endif
+            
         @empty
             <p>Nenhum Livro Cadastrado</p>
         @endforelse
