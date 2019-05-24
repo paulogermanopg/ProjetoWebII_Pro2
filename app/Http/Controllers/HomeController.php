9c87fbf365b3cfd73d912dee4d43f040f6bb24e2
Paulo Germano <paulogermano.123pg@hotmail.com>
2019-05-24 00:47:35 -0300

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\livros;
-use App\reservas;
 
 class HomeController extends Controller
 {
@@ -23,7 +22,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index(reservas $livros)
+    public function index(livros $livros)
     {
       $livros_livros = $livros -> all();
 
